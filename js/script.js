74685e693a9dd46726df0bc7b16e0cb14757463a
VikitorXD <rm551886@fiap.com.br>
2023-05-02 22:59:08 -0300

    MEXENDO COM O BOTÃO

--- js/script.js
+++ js/script.js
@@ -17,5 +17,51 @@
 
 
 //Recuperar um elemento do HTML com a função getElementById(parâmetro)
-const mn = document.getElementById("menu");
-console.log(mn.textContent);
+// const mn = document.getElementById("menu");
+// console.log(mn.textContent);
+
+// recuperando uma collection de elementos ...
+
+const aElements = document.getElementsByTagName("a");
+
+const aElementsArray = [...aElements];
+
+aElementsArray.forEach( (a)=> {
+    console.log(a);
+});
+
+
+// declarando um arry em Javascript
+
+let nr1 = [1,2,3,4,5];
+let nr2 = [6,7,8,9,10];
+// let nr3 = [nr1,nr2]
+console.log(nr1);
+
+
+// OPERADOR SPREAD(SIGINIFICA ESPALHAR)...
+let nr3 = [...nr1,...nr2]
+console.log("novo array " + nr3);
+
+nr3.forEach( (numero)=>{
+    console.log("item do array: " + numero);
+} );
+
+// console.log("novo array" + nr3);
+// nr2.push(11);
+// console.table(nr1);
+
+const imgElements = [...document.getElementsByTagName("img")];
+
+// const imgElementsArray =  [...imgElements];
+
+imgElements.forEach( (img)=>{
+    console.log(`Path das imagens: ${img.src}`);
+});
+
+const botao = document.querySelector("div >button");
+console.log(botao);
+
+botao.addEventListener("click",()=> {
+    alert("TCHAU!!")
+});

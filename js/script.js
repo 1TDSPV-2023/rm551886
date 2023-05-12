9d018ab7ba16504547a61703674dca832740c886
VikitorXD <rm551886@fiap.com.br>
2023-05-11 22:09:23 -0300

    ;

--- js/script.js
+++ js/script.js
@@ -19,96 +19,96 @@
 // const mn = document.getElementById("menu");
 // console.log(mn.textContent);
 
-// recuperando uma collection de elementos ...
-
+// //Recuperando uma collection de elementos...
 // const aElements = document.getElementsByTagName("a");
-
 // const aElementsArray = [...aElements];
 
-// aElementsArray.forEach((a) => {
-//   console.log(a);
+// aElementsArray.forEach((a)=>{
+//     console.log(a);
 // });
 
-// // declarando um arry em Javascript
-
-// let nr1 = [1, 2, 3, 4, 5];
-// let nr2 = [6, 7, 8, 9, 10];
-// // let nr3 = [nr1,nr2]
-// console.log(nr1);
-
-// // OPERADOR SPREAD(SIGINIFICA ESPALHAR)...
-// let nr3 = [...nr1, ...nr2];
-// console.log("novo array " + nr3);
-
-// nr3.forEach((numero) => {
-//   console.log("item do array: " + numero);
-// });
-
-// console.log("novo array" + nr3);
-// nr2.push(11);
-// console.table(nr1);
-
+// //Exercícios
+// //1 - Recupere uma coleção de imagens da página e imprima o
+// // atributo src...
 // const imgElements = [...document.getElementsByTagName("img")];
+// // const imgElementsArray = [...imgElements];
+// // console.log(imgElements);
 
-// // const imgElementsArray =  [...imgElements];
-
-// imgElements.forEach((img) => {
-//   console.log(`Path das imagens: ${img.src}`);
+// //IMPRIMINDO O ATRIBUTO SRC do elemento com o forEach
+// imgElements.forEach((img)=>{
+//     console.log(`Path das imagens : ${img.src}`);
 // });
 
-// const botao = document.querySelector("div >button");
-// console.log(botao);
+// const botao = document.querySelector("div > button");
 
-// // botao.addEventListener("click", () => {
-// //   alert("TCHAU!!");
-// // });
+// botao.addEventListener("click", ()=>{
+//     alert("TCHAU!!!");
+// });
 
-// // Inserir intens ao final do array com o método push;
+// //Declarando um array em Javascript
+// let nr1 = [1,2,3,4,5];
+// let nr2 = [6,7,8,9,10];
+// // let nr3 = [nr1,nr2];
+// console.log(nr1);
+// console.log(nr2);
+
+// //OPERADOR SPREAD ...
+// let nr3 = [...nr1,...nr2];
+// console.log("NOVO ARRAY " + nr3);
+
+// nr3.forEach( (numero)=>{
+//     console.log("ITEM DO ARRAY : " + numero);
+// } );
+
+// let frutas = ["laranja","banana","acerola","abacate","caqui","uva"];
 // console.log(frutas);
+// //Inserir itens ao final do array com o método push(iten);
 // frutas.push("kiwi");
-
-// // Inserir intens ao começo do array com o método unshift;
+// console.log(frutas);
+// //Inserir itens no início do array com o unshift();
 // frutas.unshift("melancia");
 // console.log(frutas);
-
-// // // remover intens do final do array com o método pop;
-// console.log(frutas);
+// //Remover itens do final do array com o método pop();
 // frutas.pop();
-
-// // // remover intens do começo do array com o método shift;
 // console.log(frutas);
+// //Remover itens do inicio do array com o método shift();
 // frutas.shift();
+// console.log(frutas);
 
-// // Realizando a busca de um intem ano array usando método indeOof(iten)
-// // quando usamos o indexOF(iten) ele retorna o indice do item procurad.
+// //Realizando a busca de um item no array usando o método indexOf(iten)
+// //Quando usamos o indexOf(iten) ele retorna o indice do item procurado.
+// // let indice = frutas.indexOf("acerola");
+// // console.log(frutas[indice]);
 
-// // // Utilizando o método splice(indice do item, qtd de vezes que ele será
-// // //removido), para remover o item do array em conjunto com o indexof(iten).
+// //Utilizando o método splice(indice do item, qtd de vezes que ele será removido), para remover um iten do array em conjunto com o indexOf(iten).
 // let indice = frutas.indexOf("abacate");
-
-// console.log("FRUTAS QUE ESTAVA NO INDICE ANTES DA REMOÇÃO: " + frutas[indice]);
-// frutas.splice(indice, 1);
-// console.log("FRUTAS QUE FICOU NO INDICE ANTES DA REMOÇÃO: " + frutas[indice]);
+// console.log("FRUTA QUE ESTAVA NO INDICE ANTES DA REMOÇÃO : " + frutas[indice]);
+// frutas.splice(indice,1);
+// console.log("FRUTA QUE FICOU NO INDICE DEPOIS DA REMOÇÃO : " + frutas[indice]);
 // console.table(frutas);
 
-// // // estrutura de repetição para interação de collections
-// // let frutas = ["laranja", "banana", "acerola", "abacate", "caqui", "uva"];
+//Estrutura de repetição para iteração de collections
+let frutas = ["laranja", "banana", "acerola", "abacate", "caqui", "uva"];
+const aElements = document.querySelectorAll("a");
+//Loop for
+// for(let x = 0; x < aElements.length ; x++){
+//     console.log("Valor de x = " + aElements[x].textContent);
+// }
+// for (const fruta of frutas) {
+//     console.log("Frutas " + fruta);
+// }
 
-// // // // loop for
-// // // for (let x = 0; x < frutas.length; x++) {
-// // //   console.log("valor do X = " + x);
-// // // }
+// for (const indice in frutas) {
+//     console.log("Fruta " + frutas[indice]);
+// }
 
-// // // for (const frutas of frutas) {
-// // //   console.log("frutas" + frutas);
-// // // }
-
-// // frutas.map((fruta, key) => {
-// //   console.log(`fruta nr ${key + 1} ${frutas}`);
+//MAP
+// frutas.map( (fruta,key)=>{
+//     console.log(`Fruta nr:${key+1} ${fruta}`);
 // });
 
-const iputUser = document.querySelectorAll("#idUser");
+const inputUser = document.querySelector("#idUser");
 
-inputUser.addEventListner("focus", () => {
-  inputUser.setAttribute("style", "outline-color: #ff0000;");
+inputUser.addEventListener("focus", () => {
+  inputUser.setAttribute("style", "outline-color:#ff0000;");
 });
